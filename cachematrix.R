583b7e7c04176b30f463b3e69e2f3c35997bf027
masvli <sven.mader+github@gmail.com>
2014-09-16 07:42:17 +0200

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv_mat <- NULL #set default value for inverse matrix variable
+	
+	# function to set content of x + check if matrix changed. 
+	set <- function(new_mat = matrix()) {
+		
+		# check matrix, if identical to existing, no need for in_mat reset
+		if (!identical(x, new_mat)) {
+			inv_mat <<- NULL #new_mat and x differ, so inverse has to be re-calculated.
+		}
+		
+		# set matrix
+		x <<- new_mat
+	}
+	get <- function() x
+	setinverse <- function(solve) inv_mat <<- solve
+	getinverse <- function() inv_mat
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -12,4 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)){
+        	message("... get chached data ...")
+        	return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setmean(inv)
+        inv
 }
